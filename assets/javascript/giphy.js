44bb491d93f5284cd47b314a86ac501511964891
Brady Douthit <brady.douthit99@gmail.com>
2019-06-19 13:35:18 -0400

    Started getting data from an array on click

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,1 +1,9 @@
 var topics = ["Goats", "Cats", "Dogs", "Apes"];
+
+$(".test").on("click", function(){
+for (var i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+    var p = $("<p>").html(topics[i]);
+    $("#test").prepend(p);
+}
+});
