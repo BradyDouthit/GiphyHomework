21fea763cd9029503c9825d03cad2f0090da9863
Brady Douthit <brady.douthit99@gmail.com>
2019-06-19 17:34:48 -0400

    Began working on getting results from Giphy.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,22 @@
-var topics = ["Goats", "Cats", "Dogs", "Apes"];
+var topics = ["Goats", "Cats", "Dogs", "Gorillas"];
 
-$(".test").on("click", function(){
-for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
-    var p = $("<p>").html(topics[i]);
-    $("#test").prepend(p);
-}
+function animalFunction() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+        var newButton = $("<button>").html(topics[i]);
+        newButton.addClass("animals");
+        $("#button-holder").prepend(newButton);
+    }
+};
+animalFunction();
+$(".animals").on("click", function() {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=xRK1KaHGDNPmpkTkb0GWbJVWI9TUgiEu&q=" +
+    animal + "&limit=25&offset=0&rating=G&lang=en";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.data[0].images.downsized)
+    })
 });
