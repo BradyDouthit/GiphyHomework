c16bb94d70ce161a0ea2042e1cf9b87ae9a9c1d8
Brady Douthit <brady.douthit99@gmail.com>
2019-06-22 12:20:55 -0400

    The search button now functions and images show.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -155,7 +155,7 @@ $("#submit").on("click", function() {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var imageCount = 0;
+        //var imageCount = 0;
         var results = response.data;
         var endOfArray = topics[topics.length - 1];
         console.log(results);
@@ -164,6 +164,126 @@ $("#submit").on("click", function() {
         var newButton = $("<button>").html(endOfArray);
         newButton.addClass("animal-buttons");
         newButton.attr("data-animal", endOfArray);
-        $("#button-holder").prepend(newButton);
+        $("#button-holder").append(newButton);
+        $("button").on("click", function() {
+            var imageCount = 0;
+            var clickCheck = 0;
+            for (var i = 0; i < results.length; i++) {
+                imageCount++
+                console.log(imageCount);
+                var newDiv = $("<div>");
+                var newImage = $("<img>");
+                newImage.attr("src", results[i].images.original_still.url);
+                newImage.addClass("image" + imageCount);
+    
+                var p = $("<p>");
+                p.html("Rating: " + results[i].rating);
+    
+                newDiv.append(p);
+                newDiv.append(newImage);
+                $("#image-holder").prepend(newDiv);
+    
+            };
+            $(".image10").on("click", function() {
+                $(this).attr("src", results[9].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[9].images.original_still.url);
+                }
+            });
+            $(".image9").on("click", function() {
+                $(this).attr("src", results[8].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[8].images.original_still.url);
+                }
+            });
+            $(".image8").on("click", function() {
+                $(this).attr("src", results[7].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[7].images.original_still.url);
+                }
+            });
+            $(".image7").on("click", function() {
+                $(this).attr("src", results[6].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[6].images.original_still.url);
+                }
+            });
+            $(".image6").on("click", function() {
+                $(this).attr("src", results[5].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[5].images.original_still.url);
+                }
+            });
+            $(".image5").on("click", function() {
+                $(this).attr("src", results[4].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[4].images.original_still.url);
+                }
+            });
+            $(".image4").on("click", function() {
+                $(this).attr("src", results[3].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[3].images.original_still.url);
+                }
+            });
+            $(".image3").on("click", function() {
+                $(this).attr("src", results[2].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[2].images.original_still.url);
+                }
+            });
+            $(".image2").on("click", function() {
+                $(this).attr("src", results[1].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[1].images.original_still.url);
+                }
+            });
+            $(".image1").on("click", function() {
+                $(this).attr("src", results[0].images.original.url);
+                if (clickCheck === 0) {
+                    clickCheck++
+                }
+                else {
+                    clickCheck--
+                    $(this).attr("src", results[0].images.original_still.url);
+                }
+            });
+        });
     });
 });
